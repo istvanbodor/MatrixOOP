1efbfac9d50cf3c34772ca3c3b3329ed2a73a8ea
Bodor István <istvanbodor19@gmail.com>
2022-10-20 10:02:31 +0200

    matrixok

---
+++ src/hu/petrik/matrixoop/Main.java
@@ -0,0 +1,15 @@
+package hu.petrik.matrixoop;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Matrix m1 = new Matrix(5,7);
+        System.out.println(m1);
+        System.out.println();
+        Matrix m2 = new Matrix(2,3);
+        System.out.println(m2);
+        System.out.println(m2.getElemekOsszege());
+        System.out.println(m2.getLegnagyobbElem());
+        System.out.println(m2.sorOsszeg(1));
+    }
+}
